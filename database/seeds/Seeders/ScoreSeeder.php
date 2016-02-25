a0265bdfaea09727202e353158752c842af519c5
Hugh <hcmpercival@gmail.com>
2016-02-25 00:05:46 +0000

    score seeder class

--- database/seeds/Seeders/ScoreSeeder.php
+++ database/seeds/Seeders/ScoreSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Faker\Provider\Biased as Biased;
 
 class ScoreSeeder extends Seeder
 {
@@ -11,6 +13,69 @@ class ScoreSeeder extends Seeder
      */
     public function run()
     {
-        //
+      DB::table('osu_scores')->delete();
+      DB::table('osu_scores_high')->delete();
+      $beatmaps = App\Models\Beatmap::orderByRaw('RAND()')->get();
+      $beatmapCount = count($beatmaps);
+      $faker = Faker::create();
+
+      if ($beatmapCount < 1) {
+        $this->command->error("Unable to seed scores due to not having enough beatmap data.");
+        return;
+      }
+
+      $users = App\Models\User::all();
+      App\Models\Score\Model::unguard();
+
+      $osuBeatmaps = $beatmaps->where('playmode',0)->take(20);
+      $taikoBeatmaps = $beatmaps->where('playmode',1)->take(20);
+      $fruitsBeatmaps = $beatmaps->where('playmode',2)->take(20);
+      $maniaBeatmaps = $beatmaps->where('playmode',3)->take(20);
+
+      $allBeatmapSets = App\Models\BeatmapSet::all();
+
+      foreach ($users as $u) {
+        //add 20 osu! Standard scores
+        foreach ($osuBeatmaps as $bm) {
+          $bms = $allBeatmapSets->find($bm->beatmapset_id);
+          $maxcombo = rand(1,5000);
+          $possible_mods = [16,24,64,72]; // hr, hd/hr, dt, hd/dt
+          $sc = App\Models\Score\Osu::create([
+            'user_id' => $u->user_id,
+            'beatmap_id' => $bm->beatmap_id,
+            'beatmapset_id' => $bm->beatmapset_id,
+            'score' => rand(50000,100000000),
+            'maxcombo' => $maxcombo,
+            'rank' => rand(1,1000),
+            'count300' => round($maxcombo*0.8),
+            'count100' => rand(0, round($maxcombo*0.15)),
+            'count50' => rand(0, round($maxcombo*0.05)),
+            'countgeki' => round($maxcombo*0.3),
+            'countmiss' => round($maxcombo*0.05),
+            'countkatu' => round($maxcombo*0.05),
+            'enabled_mods' => $possible_mods[array_rand($possible_mods)],
+            'date' => rand(1451606400, time()), // random timestamp between 01/01/2016 and now,
+          ]);
+
+          $sc2 = App\Models\Score\Best\Osu::create([
+            'user_id' => $u->user_id,
+            'beatmap_id' => $bm->beatmap_id,
+            'beatmapset_id' => $bm->beatmapset_id,
+            'score' => rand(50000,100000000),
+            'maxcombo' => $maxcombo,
+            'rank' => rand(1,1000),
+            'count300' => round($maxcombo*0.8),
+            'count100' => rand(0, round($maxcombo*0.15)),
+            'count50' => rand(0, round($maxcombo*0.05)),
+            'countgeki' => round($maxcombo*0.3),
+            'countmiss' => round($maxcombo*0.05),
+            'countkatu' => round($maxcombo*0.05),
+            'enabled_mods' => $possible_mods[array_rand($possible_mods)],
+            'date' => rand(1451606400, time()), // random timestamp between 01/01/2016 and now,
+            'pp' => 727-$faker->biasedNumberBetween(10,727),
+          ]);
+        }
+      }
+      App\Models\Score\Model::reguard();
     }
 }
